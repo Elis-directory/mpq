39dad083dc3576bd643897b24e4c1bd75f7c9788
Elis-directory <150942395+Elis-directory@users.noreply.github.com>
2023-12-29 09:35:53 -0800

    added code for adding colors based of hex values

--- MPQ/ContentView.swift
+++ MPQ/ContentView.swift
@@ -4,7 +4,15 @@
 //
 
 import SwiftUI
+extension Color {
+    init(hex: UInt) {
+        let red = Double((hex >> 16) & 0xFF) / 255.0
+        let green = Double((hex >> 8) & 0xFF) / 255.0
+        let blue = Double(hex & 0xFF) / 255.0
 
+        self.init(red: red, green: green, blue: blue)
+    }
+}
 struct ContentView: View {
     @State private var isSheetPresented = false
     @State private var itemTitle = ""
@@ -33,7 +41,11 @@ struct ContentView: View {
     @State private var queuedItem = QueuedItem()
     @State private var queuedItem2 = QueuedItem()// Create an instance
     @State private var queuedItem3 = QueuedItem()// Create an instance
-    
+    let backgroundColor = Color(hex: 0xFFFFFF)
+    let urgentColor = Color(hex: 0xE84258)
+    let semiUrgentColor = Color(hex: 0xFEE191)
+    let nonUrgentColor = Color(hex: 0xB0D8A4)
+   
     
     var body: some View {
         GeometryReader { geometry in
@@ -45,20 +57,20 @@ struct ContentView: View {
                 queuedItem.display() .onAppear {
                     queuedItem.containerWidth = Double(geometry.size.width) // Update containerWidth
                     queuedItem.containerHeight = Double(geometry.size.height) * 0.1 // Update containerHeight
-                    queuedItem.backgroundColor = Color.red
+                    queuedItem.backgroundColor = urgentColor
                 }
                 queuedItem2.display()
                     .onAppear {
                     queuedItem2.containerWidth = Double(geometry.size.width) // Update containerWidth
                     queuedItem2.containerHeight = Double(geometry.size.height) * 0.1 // Update containerHeight
-                    queuedItem2.backgroundColor = Color.yellow
+                    queuedItem2.backgroundColor = semiUrgentColor
                 }
                 
                 queuedItem3.display()
                     .onAppear {
                     queuedItem3.containerWidth = Double(geometry.size.width) // Update containerWidth
                     queuedItem3.containerHeight = Double(geometry.size.height) * 0.1 // Update containerHeight
-                    queuedItem3.backgroundColor = Color.green
+                    queuedItem3.backgroundColor = nonUrgentColor
                 }
               
                 Spacer()
@@ -74,7 +86,7 @@ struct ContentView: View {
                 }
             }
            
-        }
+        }.background(backgroundColor)
         .sheet(isPresented: $isSheetPresented) {
             VStack {
                 TextField("Title", text: $itemTitle).font(.system(size: 30)).padding()
