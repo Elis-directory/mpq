19a7f1debf6302b2243f1b9e72dcfccd9f7712bd
Elis-directory <150942395+Elis-directory@users.noreply.github.com>
2023-12-31 21:52:40 -0800

    updates to UI

--- MPQ/ContentView.swift
+++ MPQ/ContentView.swift
@@ -16,6 +16,7 @@ extension Color {
     }
 }
 
+// struct repesenting element in queuw
 struct QueuedItem {
     var title = "Title"
     var description = "Description"
@@ -43,16 +44,15 @@ struct QueuedItem {
 
 
 // UI color pallete
-let urgentColor = Color(hex: 0xE84258)
-let semiUrgentColor = Color(hex: 0xFEE191)
-let nonUrgentColor = Color(hex: 0xB0D8A4)
-let backgroundColor = Color(hex: 0xFCFCFC)
+var urgentColor = Color(hex: 0xE84258)
+var semiUrgentColor = Color(hex: 0xFEE191)
+var nonUrgentColor = Color(hex: 0xB0D8A4)
+var backgroundColor = Color(hex: 0xFCFCFC)
 
 // screen dimensions
 let screenWidth = UIScreen.main.bounds.size.width
 let screenHeight = UIScreen.main.bounds.size.height
 var modalActive = true
-
 
 
 
@@ -64,63 +64,114 @@ struct ContentView: View {
     @State private var itemDescription = ""
     @State private var queuedItem = QueuedItem()
     @State private var queuedItems: [QueuedItem] = []
+    @State private var selectedDurationIndex = 0
+    @State private var selectedUrgencyIndex = 0
     
-   
+    let urgencies = ["Urgent", "Semi-urgent", "Non-urgent"]
+    let durations = ["30 sec", "1 min", "2 min", "5 min"]
+    
     
     var body: some View {
-      
-      
-
-        VStack() {
-            Text("MPQ")
-                .font(.system(size: 60))
-                .multilineTextAlignment(.center)
-                .padding()
-            
-            ForEach(queuedItems.indices, id: \.self) { index in
-                queuedItems[index].display()
-                .onAppear {
-                    queuedItems[index].containerWidth = screenWidth
-                    queuedItems[index].containerHeight = screenHeight * 0.1
-                }.onTapGesture {
-                   
+            ZStack {
+                Image("Logo").resizable()
+                    .frame(width: screenWidth, height: screenHeight * 0.4)
+                    .offset(y: screenHeight * -0.2)
+                VStack {
+                    Rectangle()
+                        .foregroundColor(Color.blue.opacity(0))
+                        .frame(width: screenWidth, height: screenHeight * 0.25)
+                        .alignmentGuide(.top) { _ in
+                              screenHeight * 0.25
+                          }
                     
-                    if (modalActive) {
-                        queuedItems[index].containerWidth = screenWidth * 0.95
-                        queuedItems[index].containerHeight = screenHeight * 0.3
-                        modalActive = false
-                    } else {
-                        queuedItems[index].containerWidth = screenWidth
-                        queuedItems[index].containerHeight = screenHeight * 0.1
-                        modalActive = true
+                    ForEach(queuedItems.indices, id: \.self) { index in
+                        queuedItems[index].display()
+                            .onAppear {
+                                queuedItems[index].containerWidth = screenWidth
+                                queuedItems[index].containerHeight = screenHeight * 0.1
+                            }
+                            .onTapGesture {
+                                if modalActive {
+                                    queuedItems[index].containerWidth = screenWidth * 0.95
+                                    queuedItems[index].containerHeight = screenHeight * 0.3
+                                    modalActive = false
+                                } else {
+                                    queuedItems[index].containerWidth = screenWidth
+                                    queuedItems[index].containerHeight = screenHeight * 0.1
+                                    modalActive = true
+                                }
+                            }
                     }
-                   
+                    Spacer()
+                    
+                    Button(action: {
+                        isSheetPresented.toggle()
+                    }) {
+                        Image(systemName: "plus.app.fill")
+                            .font(.system(size: 60))
+                    }
+                    .padding()
                 }
-               
-           
+
+                
             }
-            Spacer()
-           
-                Button(action: {
-                    isSheetPresented.toggle()
-                }) {
-                    Image(systemName: "plus.app.fill")
-                        .font(.system(size: 60))
-                }
-                .padding()
-         
-           
-        }
-        .frame(maxWidth: .infinity, maxHeight: .infinity) // 1
+        
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(backgroundColor)
         .sheet(isPresented: $isSheetPresented) {
-            
             VStack {
+                HStack {
+                    Text("Urgency")
+                        .font(.system(size: 30))
+                        .foregroundColor(Color(hex: 0xFFC7C7CC))
+                               
+                    Spacer()
+                               
+                    HStack(spacing: 10) {
+                        Rectangle()
+                            .fill(urgentColor)
+                            .frame(width: 30, height: 30)
+                            .onTapGesture {
+                                selectedUrgencyIndex = 0
+                                        
+                            }
+                        Rectangle()
+                            .fill(semiUrgentColor)
+                            .frame(width: 30, height: 30)
+                            .onTapGesture {
+                                selectedUrgencyIndex = 1
+                            }
+                                   
+                            Rectangle()
+                                .fill(nonUrgentColor)
+                                .frame(width: 30, height: 30)
+                                .onTapGesture {
+                                    selectedUrgencyIndex = 2
+                            }
+                        }
+                               
+                               
+                    }
+                    .padding()
                
-                TextField("Title", text: $itemTitle).font(.system(size: 30)).padding()
-                TextField("Urgency", text: $itemUrgency).font(.system(size: 30)).padding()
+               
+//                TextField("Title", text: $itemTitle).font(.system(size: 30)).padding()
                 TextField("Description", text: $itemDescription).font(.system(size: 30)).padding()
                 
+                HStack {
+                    Text("Duration").font(.system(size:30)).foregroundColor(Color(hex: 0xFFC7C7CC))
+                              Spacer()
+                              Picker(selection: $selectedDurationIndex, label: Text("Duration")) {
+                                  ForEach(0..<durations.count, id: \.self) { index in
+                                      Text(durations[index]).tag(index)
+                                  }
+                              }
+                              .pickerStyle(MenuPickerStyle())
+                              .frame(width: 120)
+                          }
+                          .padding()
+              
+            
                 Spacer()
                 Button(action: {
                     isSheetPresented.toggle()
